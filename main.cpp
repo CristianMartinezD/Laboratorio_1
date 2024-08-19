010e7807924e23859e08741053504103c7d6a8c6
CristianMartinezD <cristian.martinenez@udea.edu.co>
2024-08-18 19:26:59 -0500

    Desarrolle la funcion obtener_entero(string numero_en_texto), comence a desarrollar la seccion del main(), y mejore los ejercicios 1-10

--- main.cpp
+++ main.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
+
+/* Validador de numero entero */
+int obtener_entero(string numero_en_texto){
+
+    string NUMEROS = "1234567890";
+    unsigned validador = 0;
+
+    while (validador < numero_en_texto.length())
+    {
+        for (unsigned i = 0; i < numero_en_texto.length(); i++)
+        {
+
+            if (NUMEROS.find(numero_en_texto[i]) != string::npos) {
+                validador += 1;
+            } else{
+                cout<<"Ingresa un numero valido: "; cin>>numero_en_texto;
+                validador = 0;
+                break;
+            }
+        }
+        if (validador != 0) validador += 1;
+    }
+    return stoi(numero_en_texto);
+
+}
+
+
+
+
 /*
     EJECICIO No. 1
 */
@@ -9,7 +38,9 @@ void ejercicio_1(){
     int A,B;
     cout<<"\nEscribe un numero entero: "; cin>>A;
     cout<<"\nEscribe otro numero entero: "; cin>>B;
-    cout<<"\n\nEl residuo de la division "<<A<<"/"<<B<<" es: "<<A%B;
+    cout<<"\nEl residuo de la division "<<A<<"/"<<B<<" es: "<<A%B<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -20,8 +51,10 @@ void ejercicio_1(){
 void ejercicio_2(){
     int A;
     cout<<"\nEscribe un numero entero: "; cin>>A;
-    if (A%2 == 0) {cout<<endl<<A<<" Es par";}
-    else {cout<<endl<<endl<<A<<" Es impar";}
+    if (A%2 == 0) {cout<<endl<<A<<" Es par\n\n";}
+    else {cout<<endl<<endl<<A<<" Es impar\n\n";}
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -34,7 +67,9 @@ void ejercicio_3(){
     cout<<"\nEscribe un numero entero: "; cin>>A;
     cout<<"\nEscribe otro numero entero: "; cin>>B;
     Mayor = (A > B) ? A:B;
-    cout<<"\n\nEl mayor es "<<Mayor;
+    cout<<"\nEl mayor es "<<Mayor<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -47,7 +82,9 @@ void ejercicio_4(){
     cout<<"\nEscribe un numero entero: "; cin>>A;
     cout<<"\nEscribe otro numero entero: "; cin>>B;
     menor = (A < B) ? A:B;
-    cout<<"\n\nEl menor es "<<menor;
+    cout<<"\n\nEl menor es "<<menor<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -60,8 +97,10 @@ void ejercicio_5(){
     cout<<"\nEscribe un numero entero: "; cin>>A;
     cout<<"\nEscribe otro numero entero: "; cin>>B;
     float residuo = A%B;
-    if (residuo/B >= 0.5){cout<<endl<<A<<"/"<<B<<" = "<<(A/B)+1;}
-    else {cout<<endl<<A<<"/"<<B<<" = "<<A/B;}
+    if (residuo/B >= 0.5){cout<<endl<<A<<"/"<<B<<" = "<<(A/B)+1<<endl<<endl;}
+    else {cout<<endl<<A<<"/"<<B<<" = "<<A/B<<endl<<endl;}
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -83,9 +122,11 @@ void ejercicio_6(){
         for (int var = 1; var < B; ++var) {
             resultado *= A;
         }
-        if(B_es_negativo == false)cout<<endl<<A<<"^"<<B<<" = "<<resultado;
-        else {float uno = 1; float resul = uno/resultado; cout<<endl<<A<<"^-"<<B<<" = "<<resul;}
+        if(B_es_negativo == false)cout<<endl<<A<<"^"<<B<<" = "<<resultado<<endl<<endl;
+        else {float resul = 1.0/resultado; cout<<endl<<A<<"^-"<<B<<" = "<<resul<<endl<<endl;}
     }
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -104,8 +145,10 @@ void ejercicio_7(){
     for (int var = 1; var <= N; ++var) {
         suma += var;
     }
-    if (A_es_negativo == true) {cout<<"\nLa sumatoria desde 0 hasta -"<<N<<" es -"<<suma;}
-    else cout<<"\nLa sumatoria desde 0 hasta "<<N<<" es "<<suma;
+    if (A_es_negativo == true) {cout<<"\nLa sumatoria desde 0 hasta -"<<N<<" es -"<<suma<<endl<<endl;}
+    else cout<<"\nLa sumatoria desde 0 hasta "<<N<<" es "<<suma<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -121,7 +164,9 @@ void ejercicio_8(){
     for (int var = 1; var <= N; ++var) {
         resultado *= var;
     }
-    cout<<"\n"<<N<<"!: "<<resultado;
+    cout<<"\n"<<N<<"!: "<<resultado<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -133,8 +178,10 @@ void ejercicio_9(){
     int r; float pi = 3.1416;
     cout<<"\nEscribe un numero entero positivo: "; cin>>r;
 
-    cout<<"\n\nPerimetro: "<<2*pi*r;
-    cout<<"\nArea: "<<pi*r*r;
+    cout<<"\n\nPerimetro: "<<2*pi*r<<endl;
+    cout<<"\nArea: "<<pi*r*r<<endl<<endl;
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
@@ -152,16 +199,49 @@ void ejercicio_10(){
         cout<<N*i<<endl;
         i++;
     }
+    system("pause");
+    system("cls");
 }
 /*##########FIN##########*/
 
 
 
+void pronlema_1(){}
+
 int main()
 {
-    //int opcion = 0; Para la implementación de un switch.
+    int opcion = 100;
+    while (opcion != 0) {
+        cout<<"\nDeseas probar un EJERCICIO o un PROBLEMA?\n1. Ejercicio\n2. Problema\n0. Salir";
+        cout<<"\nINGRESA UNA OPCION (1,2 o 0): "; cin>>opcion;
 
-    ejercicio_10();
+        if (opcion == 1){
+            int ejercicio;
+            cout<<"\nCual ejercicio deseas probar (del 1 al 30): "; cin>>ejercicio;
+            switch (ejercicio) {
+            case 1: ejercicio_1();break;
+            case 2: ejercicio_2();break;
+            case 3: ejercicio_3();break;
+            case 4: ejercicio_4();break;
+            case 5: ejercicio_5();break;
+            case 6: ejercicio_6();break;
+            case 7: ejercicio_7();break;
+            case 8: ejercicio_8();break;
+            case 9: ejercicio_9();break;
+            case 10: ejercicio_10();break;
+            }
+        }
+
+        else if(opcion == 2){
+            switch (opcion) {
+            case 1: pronlema_1();
+            }
+        }
+
+        else opcion = 0;
+
+    }
+
 
     return 0;
 }
