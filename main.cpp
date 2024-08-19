d207402065931f52ea041247878c5ecc9249848b
CristianMartinezD <cristian.martinenez@udea.edu.co>
2024-08-19 00:46:51 -0500

    Termine de blindar el codigo de los 10 primeros ejercicios. Ademas Blinde el menu inicial del programa

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 
 /* Validador de numero entero */
-int obtener_entero(string numero_en_texto){
+int obtener_entero(string numero_en_texto){ /* Esta función verifica si un string puede
+                                            puede servir como un numero entero */
 
     bool El_numero_es_negativo = false;
     string NUMEROS = "1234567890";
@@ -57,10 +58,13 @@ void ejercicio_1(){
     EJECICIO No. 2
 */
 void ejercicio_2(){
-    int A;
-    cout<<"\nEscribe un numero entero: "; cin>>A;
-    if (A%2 == 0) {cout<<endl<<A<<" Es par\n\n";}
-    else {cout<<endl<<endl<<A<<" Es impar\n\n";}
+    string n; int N;
+    cout<<"\nEscribe un numero entero y te dire si es par o impar: "; cin>>n;
+
+    N = obtener_entero(n); //Aquí verifico si ese string n puede servir como un entero.
+
+    if (N%2 == 0) {cout<<endl<<N<<" Es par\n\n";}
+    else {cout<<endl<<endl<<N<<" Es impar\n\n";}
     system("pause");
     system("cls");
 }
@@ -71,9 +75,12 @@ void ejercicio_2(){
     EJECICIO No. 3
 */
 void ejercicio_3(){
-    int A,B,Mayor;
-    cout<<"\nEscribe un numero entero: "; cin>>A;
-    cout<<"\nEscribe otro numero entero: "; cin>>B;
+    int A,B,Mayor; string a,b;
+    cout<<"\nEscribe dos numeros y te digo cual es mayor.\n";
+    cout<<"\nEscribe el primer numero entero: "; cin>>a;
+    A = obtener_entero(a);
+    cout<<"\nEscribe el segundo numero entero: "; cin>>b;
+    B = obtener_entero(b);
     Mayor = (A > B) ? A:B;
     cout<<"\nEl mayor es "<<Mayor<<endl<<endl;
     system("pause");
@@ -86,9 +93,12 @@ void ejercicio_3(){
     EJECICIO No. 4
 */
 void ejercicio_4(){
-    int A,B,menor;
-    cout<<"\nEscribe un numero entero: "; cin>>A;
-    cout<<"\nEscribe otro numero entero: "; cin>>B;
+    int A,B,menor; string a,b;
+    cout<<"\nEscribe dos numeros y te digo cual es menor.\n";
+    cout<<"\nEscribe el primer numero entero: "; cin>>a;
+    A = obtener_entero(a);
+    cout<<"\nEscribe el segundo numero entero: "; cin>>b;
+    B = obtener_entero(b);
     menor = (A < B) ? A:B;
     cout<<"\n\nEl menor es "<<menor<<endl<<endl;
     system("pause");
@@ -101,14 +111,31 @@ void ejercicio_4(){
     EJECICIO No. 5
 */
 void ejercicio_5(){
-    int A,B;
-    cout<<"\nEscribe un numero entero: "; cin>>A;
-    cout<<"\nEscribe otro numero entero: "; cin>>B;
+    int A,B; string a,b;
+    cout<<"\n     REDONDEO DE A/B\n";
+    cout<<"\nEscribe un numero entero: "; cin>>a;
+    A = obtener_entero(a);
+    cout<<"\nEscribe otro numero entero: "; cin>>b;
+    B = obtener_entero(b);
+
+    if (B<0) {B = -1*B; A = -1*A;}
+
     float residuo = A%B;
-    if (residuo/B >= 0.5){cout<<endl<<A<<"/"<<B<<" = "<<(A/B)+1<<endl<<endl;}
-    else {cout<<endl<<A<<"/"<<B<<" = "<<A/B<<endl<<endl;}
-    system("pause");
-    system("cls");
+
+    if (residuo > 0) {
+        if (residuo/B >= 0.5){cout<<endl<<A<<"/"<<B<<" = "<<(A/B)+1<<endl<<endl;}
+        else {cout<<endl<<A<<"/"<<B<<" = "<<A/B<<endl<<endl;}
+        system("pause");
+        system("cls");
+    }
+
+    else {
+        residuo = -1*residuo;
+        if (residuo/B >= 0.5){cout<<endl<<A<<"/"<<B<<" = "<<(A/B)-1<<endl<<endl;}
+        else {cout<<endl<<A<<"/"<<B<<" = "<<A/B<<endl<<endl;}
+        system("pause");
+        system("cls");
+    }
 }
 /*##########FIN##########*/
 
@@ -117,9 +144,12 @@ void ejercicio_5(){
     EJECICIO No. 6
 */
 void ejercicio_6(){
-    int A,B;
-    cout<<"\nEscribe un numero entero: "; cin>>A;
-    cout<<"\nEscribe otro numero entero: "; cin>>B;
+    int A,B; string a,b;
+    cout<<"\n     POTENCIA DE A^B\n";
+    cout<<"\nEscribe un numero entero: "; cin>>a;
+    A = obtener_entero(a);
+    cout<<"\nEscribe otro numero entero: "; cin>>b;
+    B = obtener_entero(b);
 
     bool B_es_negativo = false;
     if (B < 0){ B = B*(-1); B_es_negativo = true;}
@@ -143,9 +173,10 @@ void ejercicio_6(){
     EJECICIO No. 7
 */
 void ejercicio_7(){
-    int N;
-    cout<<"\nEscribe un numero entero: "; cin>>N;
+    int N; string n;
+    cout<<"\nEscribe un numero entero N y te doy la sumatoria de 0 hasta N: "; cin>>n;
 
+    N = obtener_entero(n);
     bool A_es_negativo = false;
     if (N < 0) {A_es_negativo = true; N = -1*N;}
 
@@ -165,8 +196,11 @@ void ejercicio_7(){
     EJECICIO No. 8
 */
 void ejercicio_8(){
-    int N;
-    cout<<"\nEscribe un numero entero positivo: "; cin>>N;
+    int N; string n;
+    cout<<"\n     HALLEMOS N!\n";
+    cout<<"\nEscribe un numero entero positivo: "; cin>>n;
+    N = obtener_entero(n);
+    while (N < 0) {cout<<"El numero debe ser positivo.\n"; N = obtener_entero("NO VALIDO");}
 
     int long long resultado = 1;
     for (int var = 1; var <= N; ++var) {
@@ -183,11 +217,13 @@ void ejercicio_8(){
     EJECICIO No. 9
 */
 void ejercicio_9(){
-    int r; float pi = 3.1416;
+    int R; float pi = 3.1416; string r;
+    cout<<"\n     HALLAR PERIMETRO Y AREA.\n";
     cout<<"\nEscribe un numero entero positivo: "; cin>>r;
-
-    cout<<"\n\nPerimetro: "<<2*pi*r<<endl;
-    cout<<"\nArea: "<<pi*r*r<<endl<<endl;
+    R = obtener_entero(r);
+    while (R < 0) {cout<<"El numero debe ser positivo.\n"; R = obtener_entero("NO VALIDO");}
+    cout<<"\n\nPerimetro: "<<2*pi*R<<endl;
+    cout<<"\nArea: "<<pi*R*R<<endl<<endl;
     system("pause");
     system("cls");
 }
@@ -198,8 +234,12 @@ void ejercicio_9(){
     EJECICIO No. 10
 */
 void ejercicio_10(){
-    int N;
-    cout<<"\nEscribe un numero mayor que 0: "; cin>>N;
+    int N; string n;
+    cout<<"\n     MULTIPLOS DE N MENORES A 100\n";
+    cout<<"\nEscribe un numero mayor que 0: "; cin>>n;
+    N = obtener_entero(n);
+    while (N < 0) {cout<<"El numero debe ser positivo.\n"; N = obtener_entero("NO VALIDO");}
+
     cout<<"\nMultiplos de "<<N<<" menores que 100:\n";
 
     int i = 1;
@@ -218,12 +258,17 @@ void pronlema_1(){}
 
 int main()
 {
-    int opcion = 100;
-    while (opcion != 0) {
+    int OPCION = 100;
+    while (OPCION != 0) {
+        string opcion;
         cout<<"\nDeseas probar un EJERCICIO o un PROBLEMA?\n1. Ejercicio\n2. Problema\n0. Salir";
         cout<<"\nINGRESA UNA OPCION (1,2 o 0): "; cin>>opcion;
+        OPCION = obtener_entero(opcion);
+        while (OPCION != 1 && OPCION != 2 && OPCION != 0) {
+            OPCION = obtener_entero("NO VALIDO");
+        }
 
-        if (opcion == 1){
+        if (OPCION == 1){
             int ejercicio;
             cout<<"\nCual ejercicio deseas probar (del 1 al 30): "; cin>>ejercicio;
             switch (ejercicio) {
@@ -240,13 +285,13 @@ int main()
             }
         }
 
-        else if(opcion == 2){
-            switch (opcion) {
+        else if(OPCION == 2){
+            switch (OPCION) {
             case 1: pronlema_1();
             }
         }
 
-        else opcion = 0;
+        else OPCION = 0;
 
     }
 
